6a31b93708831d3aa1af2900fa8bca659a1001a8
CrazyBoy49z <yura.finiv@gmail.com>
2026-05-07 21:21:11 +0300

    Fix package seo

---
+++ config/lazy-seo-defaults.php
@@ -0,0 +1,221 @@
+<?php
+
+return [
+    'features' => [
+        'meta' => true,
+        'schema' => true,
+        'redirects' => true,
+        'sitemap' => true,
+        'crawler' => true,
+        'monitoring' => true,
+        'indexnow' => true,
+        'content_intelligence' => true,
+        'og_image' => true,
+        'livewire' => true,
+        'admin' => true,
+        'api' => true,
+    ],
+
+    'tables' => [
+        'seo' => 'seo',
+        'seo_redirects' => 'seo_redirects',
+        'seo_templates' => 'seo_templates',
+        'seo_scans' => 'seo_scans',
+        'seo_scan_issues' => 'seo_scan_issues',
+        'seo_indexing_logs' => 'seo_indexing_logs',
+    ],
+
+    'defaults' => [
+        'title' => 'Laravel',
+        'description' => '',
+        'keywords' => '',
+        'canonical_url' => null,
+        'robots' => ['index', 'follow'],
+        'image' => null,
+        'type' => 'website',
+    ],
+
+    'routes' => [
+        'web' => false,
+        'admin_prefix' => 'lazy-seo',
+        'admin_middleware' => ['web'],
+        'api' => false,
+        'api_prefix' => 'seo',
+        'api_middleware' => ['api'],
+        'api_write_middleware' => [],
+        'api_allow_morph_binding' => false,
+    ],
+
+    'redirects' => [
+        'enabled' => true,
+        'preserve_query' => true,
+        'regex_enabled' => true,
+        'wildcard_enabled' => true,
+        'cache_seconds' => 60,
+        'allowed_status_codes' => [301, 302, 307, 308, 410],
+    ],
+
+    'sitemap' => [
+        'path' => 'sitemap.xml',
+        'index_path' => 'sitemap.xml',
+        'cache_key' => 'lazy-seo.sitemap',
+        'cache_store' => null,
+        'cache_minutes' => 60,
+        'cache_tags_enabled' => false,
+        'cache_tags' => ['lazy-seo', 'sitemap'],
+        'default_change_frequency' => 'weekly',
+        'default_priority' => 0.8,
+        'chunk_size' => 50000,
+        'max_urls' => 0,
+        'gzip' => false,
+        'force_index' => false,
+        'exclude' => [
+            'admin/*',
+            'nova/*',
+            'horizon/*',
+            'telescope/*',
+        ],
+        'static_urls' => [],
+        'models' => [],
+    ],
+
+    'cache' => [
+        'resolved_minutes' => 0,
+    ],
+
+    'templates' => [
+        'enabled' => true,
+        'default' => null,
+    ],
+
+    'og_image' => [
+        'disk' => 'public',
+        'directory' => 'og',
+        'width' => 1200,
+        'height' => 630,
+    ],
+
+    'webhooks' => [
+        'seo.created' => null,
+        'seo.updated' => null,
+        'seo.deleted' => null,
+    ],
+
+    'schema' => [
+        'organization' => [
+            'logo' => null,
+            'same_as' => [],
+        ],
+        'website' => [
+            'search_url' => null,
+        ],
+    ],
+
+    'queue' => [
+        'enabled' => true,
+        'connection' => null,
+        'queue' => 'default',
+        'chunk_pages' => 25,
+        'tries' => 2,
+        'timeout' => 600,
+    ],
+
+    'history' => [
+        'enabled' => true,
+        'trend_limit' => 10,
+        'store_regressions' => true,
+    ],
+
+    'audit' => [
+        'severity_weights' => [
+            'error' => 8,
+            'warning' => 4,
+            'notice' => 1,
+        ],
+        'max_score_penalty' => 100,
+        'checks' => [
+            'http_error' => true,
+            'missing_title' => true,
+            'title_length' => true,
+            'duplicate_title' => true,
+            'missing_description' => true,
+            'description_length' => true,
+            'duplicate_description' => true,
+            'missing_h1' => true,
+            'multiple_h1' => true,
+            'missing_canonical' => true,
+            'canonical_conflict' => true,
+            'noindex' => true,
+            'missing_image_alt' => true,
+            'broken_link' => true,
+            'broken_external_link' => true,
+            'redirect_chain' => true,
+            'orphan_page' => true,
+        ],
+    ],
+
+    'monitoring' => [
+        'enabled' => true,
+        'url' => null,
+        'schedule' => null,
+        'scheduled_queue' => false,
+        'max_pages' => 50,
+        'fail_under' => 75,
+        'pass_score' => 75,
+        'keep_scans' => 100,
+    ],
+
+    'crawler' => [
+        'enabled' => true,
+        'max_pages' => 50,
+        'timeout' => 10,
+        'user_agent' => 'LazySeoBot/1.0',
+        'respect_noindex' => false,
+        'check_external_links' => false,
+        'max_external_links' => 50,
+        'exclude' => [
+            'admin/*',
+            'nova/*',
+            'horizon/*',
+            'telescope/*',
+            'login',
+            'logout',
+        ],
+    ],
+
+    'indexnow' => [
+        'enabled' => false,
+        'key' => null,
+        'key_location' => null,
+        'endpoint' => 'https://api.indexnow.org/indexnow',
+        'host' => null,
+        'timeout' => 10,
+        'retry_times' => 2,
+        'retry_sleep' => 250,
+        'chunk_size' => 1000,
+        'log' => true,
+    ],
+
+    'alerts' => [
+        'enabled' => false,
+        'score_threshold' => 75,
+        'critical_issues_threshold' => 1,
+        'new_issues_threshold' => 1,
+        'failed_scans' => true,
+        'cooldown_minutes' => 60,
+        'cache_store' => null,
+        'webhook_url' => null,
+        'include_issues_limit' => 10,
+    ],
+
+    'content_intelligence' => [
+        'enabled' => true,
+        'min_words' => 300,
+        'max_keyword_density' => 3.5,
+        'min_keyword_density' => 0.3,
+        'max_readability_sentence_words' => 22,
+        'internal_link_minimum' => 1,
+    ],
+
+    'ai_token' => null,
+];
